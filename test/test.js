72b4561408266652ffaebe6d11db137d38f1dcb6
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-03-06 17:18:16 +0530

    add tests

---
+++ test/test.js
@@ -0,0 +1,30 @@
+
+var Notifier = require('../');
+
+exports.testConfig = function (test) {
+  var notifier = new Notifier();
+
+  notifier.use({
+    APN: false,
+    facebook: true,
+    email: true,
+    actions: ['comment', 'like'],
+    tplPath: require('path').resolve(__dirname, './templates')
+  });
+
+  var comment = {
+    to: 'Tom',
+    from: 'Harry'
+  }
+
+  notifier.send('comment', {
+    to: 'to@madhums.me',
+    subject: 'ab ad a asd',
+    from: 'harry@madhums.me',
+    locals: comment // should be the object containing the objects used in the templates
+  });
+
+  test.expect(1);
+  test.ok(true, "this assertion should pass");
+  test.done();
+}
